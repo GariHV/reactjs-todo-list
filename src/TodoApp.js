56f0e14741c8e029c642e262b2a4ddaba64d0e66
rogerpuig <rogerpuigdemasa@gmail.com>
2022-02-18 13:26:26 +0100

    estados 2

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -148,3 +148,39 @@ export const TodoApp = () => {
         </div>
     )
 }
+
+
+function pokedex(){
+    return fetch('../public/evolucines.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
+
+function revisarPokemon(state){
+    if(state["exp"]>= 10){
+        if(state["lvl"]===0){
+            npokemon=Math.floor(Math.random() * (151 - 1)) + 1;
+            const pokedex=pokedex()
+            state["infoPoke"]=pokedex[npokemon]
+            state["poke"]=state["infoPoke"][1]
+        }
+        else if(state["lvl"]>0){
+            if(state["poke"]===state["infoPoke"]["1"]){
+                if(Object.keys(state["infoPoke"]).length>2){
+                    if(state["lvl"]>=state["infoPoke"]["lvl1"]){
+                        state["poke"]=state["infoPoke"]["2"]
+                    }
+                }
+            }
+            if(state["poke"]===state["infoPoke"]["2"]){
+                if(Object.keys(state["infoPoke"]).length>4){
+                    if(state["lvl"]>=state["infoPoke"]["lvl2"]){
+                        state["poke"]=state["infoPoke"]["3"]
+                    }
+                }
+            }
+        }
+    }
+}
+
--- src/estadoReduce.js
+++ src/estadoReduce.js
@@ -1,61 +1,35 @@
 export const estadoReduce = (state, action) => {
 
-    function pokedex(){
-        return fetch('../public/evolucines.json')
-      .then(response => response.json())
-      .then(data => console.log(data));
-    }
-
     switch (action) {
-        case "facil":
+        case "facil-true":
             state["oro"]= state["oro"]+1
             state["exp"]= state["exp"]+1
+            setestado(state)
             break
-        case "intermedio":
+        case "intermedio-true":
             state["oro"]= state["oro"]+2
             state["exp"]= state["exp"]+2
+            setestado(state)
             break
-        case "dificil":
+        case "dificil-true":
             state["oro"]= state["oro"]+3
             state["exp"]= state["exp"]+3
+            setestado(state)
             break
-        case "facil-":
+        case "facil-false":
             state["oro"]= state["oro"]-1
             state["exp"]= state["exp"]-1
+            setestado(state)
             break
-        case "intermedio-":
+        case "intermedio-false":
             state["oro"]= state["oro"]-2
             state["exp"]= state["exp"]-2
+            setestado(state)
             break
-        case "dificil-":
+        case "dificil-false":
             state["oro"]= state["oro"]-3
             state["exp"]= state["exp"]-3
-            break
-        case "subirlvl":
-            if(state["exp"]>= 10){
-                if(state["lvl"]===0){
-                    npokemon=Math.floor(Math.random() * (151 - 1)) + 1;
-                    const pokedex=pokedex()
-                    state["infoPoke"]=pokedex[npokemon]
-                    state["poke"]=state["infoPoke"][1]
-                }
-                else if(state["lvl"]>0){
-                    if(state["poke"]===state["infoPoke"]["1"]){
-                        if(Object.keys(state["infoPoke"]).length>2){
-                            if(state["lvl"]>=state["infoPoke"]["lvl1"]){
-                                state["poke"]=state["infoPoke"]["2"]
-                            }
-                        }
-                    }
-                    if(state["poke"]===state["infoPoke"]["2"]){
-                        if(Object.keys(state["infoPoke"]).length>4){
-                            if(state["lvl"]>=state["infoPoke"]["lvl2"]){
-                                state["poke"]=state["infoPoke"]["3"]
-                            }
-                        }
-                    }
-                }
-            }
+            setestado(state)
             break
         default:
     }
